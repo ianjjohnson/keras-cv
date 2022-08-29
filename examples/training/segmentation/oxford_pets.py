86d6749cdcc946b8ea457800c355786ef362a418
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-08-29 16:38:41 -0600

    Example is now working

--- examples/training/segmentation/oxford_pets.py
+++ examples/training/segmentation/oxford_pets.py
@@ -22,6 +22,7 @@ from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow.keras import metrics
 from tensorflow.keras import optimizers
+from matplotlib import pyplot as plt
 
 import keras_cv
 from keras_cv import models
@@ -36,13 +37,13 @@ flags.DEFINE_string(
 flags.DEFINE_string(
     "tensorboard_path", None, "Directory which will be used to store tensorboard logs."
 )
-flags.DEFINE_integer("batch_size", 256, "Batch size for training and evaluation.")
+flags.DEFINE_integer("batch_size", 64, "Batch size for training and evaluation.")
 flags.DEFINE_boolean(
     "use_xla", True, "Whether or not to use XLA (jit_compile) for training."
 )
 flags.DEFINE_float(
     "initial_learning_rate",
-    0.1,
+    0.0005,
     "Initial learning rate which will reduce on plateau.",
 )
 
@@ -52,9 +53,7 @@ FLAGS(sys.argv)
 
 CLASSES = 2
 IMAGE_SIZE = (128, 128)
-EPOCHS = 250
-
-tmp = None
+EPOCHS = 20
 
 
 @tf.function
@@ -98,14 +97,14 @@ strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
     model = models.UNet(
         include_rescaling=True,
-        block_widths=[32, 64, 96, 128],
+        block_widths=[32, 64, 96],
         output_channels=CLASSES,
         input_shape=IMAGE_SIZE + (3,),
         output_activation="softmax",
     )
 
-optimizer = optimizers.SGD(learning_rate=FLAGS.initial_learning_rate, momentum=0.9)
-loss_fn = losses.CategoricalCrossentropy(label_smoothing=0.1)
+optimizer = optimizers.Adam(learning_rate=FLAGS.initial_learning_rate, global_clipnorm=10)
+loss_fn = losses.CategoricalCrossentropy()
 
 with strategy.scope():
     training_metrics = [metrics.CategoricalAccuracy()]
@@ -134,3 +133,20 @@ model.fit(
     callbacks=callbacks,
     validation_data=test_ds,
 )
+
+def visualize_predictions(dataset, title):
+    plt.figure(figsize=(10, 10)).suptitle(title, fontsize=18)
+    for i, samples in enumerate(iter(dataset.take(9))):
+        images, masks = samples
+        plt.subplot(9, 3, 3 * i + 1)
+        plt.imshow(images[0].numpy().astype("uint8"))
+        plt.axis("off")
+        plt.subplot(9, 3, 3 * i + 2)
+        plt.imshow(tf.math.argmax(masks[0], axis=-1).numpy().astype("uint8"))
+        plt.axis("off")
+        plt.subplot(9, 3, 3 * i + 3)
+        plt.imshow(tf.math.argmax(model(tf.expand_dims(images[0], axis=0)), axis=-1).numpy()[0].astype("uint8"))
+        plt.axis("off")
+    plt.show()
+
+visualize_predictions(train_ds, "Example predictions")
