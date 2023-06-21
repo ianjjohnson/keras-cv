16dd26e946747b3bce3f84c0d519d46bd738a476
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2023-06-21 13:46:55 -0600

    Copyright notice

--- keras_cv/backend/tf_ops.py
+++ keras_cv/backend/tf_ops.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from keras_core.backend.tensorflow import *  # noqa: F403, F401
 from keras_core.backend.tensorflow.core import *  # noqa: F403, F401
 from keras_core.backend.tensorflow.math import *  # noqa: F403, F401
@@ -10,6 +23,4 @@ from tensorflow import range as arange  # noqa: F403, F401
 from tensorflow import reduce_max as max  # noqa: F403, F401
 from tensorflow import reshape  # noqa: F403, F401
 from tensorflow import split  # noqa: F403, F401
-from tensorflow.keras.preprocessing.image import (  # noqa: F403, F401
-    smart_resize,
-)
+from tensorflow.keras.preprocessing.image import smart_resize  # noqa: F403, F401
